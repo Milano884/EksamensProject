b927f5287710a14627b385a84f249ff33f0f5f7e
Milan Aleksander Mauritz Sørensen <milan.sorensen@gmail.com>
2024-04-23 10:29:34 +0200

    test

--- ZBC/Areas/Identity/Pages/Error.cshtml.cs
+++ ZBC/Areas/Identity/Pages/Error.cshtml.cs
@@ -11,7 +11,7 @@ namespace ZBC.Areas.Identity.Pages
 {
     /// <summary>
     ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-    ///     directly from your code. This API may change or be removed in future releases.
+    ///     directly from your code. This API may change or be removed in future releases
     /// </summary>
     [AllowAnonymous]
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
